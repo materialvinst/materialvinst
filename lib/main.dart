b988366c610183f1ee53348db10612250c03f03b
emanueljg <emanueljohnsongodin@gmail.com>
2024-09-21 16:02:57 +0200

    remove counter

--- lib/main.dart
+++ lib/main.dart
@@ -170,19 +170,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState gtells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -218,18 +205,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 const Icon(Icons.settings),
               ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.info),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
